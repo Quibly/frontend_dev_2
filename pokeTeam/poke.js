7d4caca5bf4217fb86ee337e551a2183ff51b0de
Quibly <trippy524@yahoo.com>
2022-06-27 10:37:35 -0600

    starting framework for pokemon app

---
+++ pokeTeam/poke.js
@@ -0,0 +1,5 @@
+document.getElementById('updated').innerHTML = `Last Updated: ${document.lastModified}`;
+
+const year = document.querySelector('#year');
+const y = new Date().getFullYear();
+year.textContent = `\xA9 ${y} | Adam Dutson | Utah`;
